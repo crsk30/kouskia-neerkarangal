cc70e946ef9760043e91b3c068808ad8c2c41819
Sai Krishna C R <sai.crsk30@gmail.com>
2025-03-01 10:34:24 +0530

    Source code after First build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -14,14 +15,21 @@ import { Router, RouterModule } from '@angular/router';
 export class LoginComponent  implements OnInit {
   loginForm: FormGroup;
   
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private platform: Platform,
+    private alertController: AlertController,
+  ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.platform.backButton.subscribeWithPriority(10, async () => {
+      this.showExitConfirm();
+    });
+  }
   onSubmit() {
     if (this.loginForm.valid) {
       console.log('Form Submitted:', this.loginForm.value);
@@ -30,7 +38,33 @@ export class LoginComponent  implements OnInit {
     }
   }
 
+  get password() {
+    return this.loginForm.get('password');
+  }
+
   hasError(controlName: string): boolean {
     const control = this.loginForm.get(controlName);
     return !!(control && control.invalid && (control.dirty || control.touched));
-  }}
+  }
+
+  async showExitConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm Exit',
+      message: 'Do you really want to exit?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Exit',
+          handler: () => {
+            (navigator as any).app.exitApp(); 
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -11,18 +12,29 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
+
+
+  
 export class SignupComponent  implements OnInit {
   signupForm: FormGroup;
   
   constructor(private fb: FormBuilder) {
     this.signupForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', Validators.required],
-      mobile: ['', Validators.required],
-      password: ['', Validators.required],
-      cpassword: ['', Validators.required]
-    });
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$'), Validators.minLength(10)]],
+      password: ['', [Validators.required,Validators.minLength(8)]],
+      cpassword: ['', [Validators.required, Validators.minLength(8)]]
+    },
+    { validators: this.matchPasswordValidator } 
+  );
   }
+
+  matchPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('cpassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  };
   ngOnInit() {}
 
   onSubmit() {
@@ -31,6 +43,26 @@ export class SignupComponent  implements OnInit {
     } else {
       this.signupForm.markAllAsTouched();
     }
+  }
+
+  get name() {
+    return this.signupForm.get('name');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get mobile() {
+    return this.signupForm.get('mobile');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get cpassword() {
+    return this.signupForm.get('cpassword');
   }
 
   hasError(controlName: string): boolean {
